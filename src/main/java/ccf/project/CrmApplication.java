058a8a32f89ed199f70dbaba854d1da198d1a1ac
inchavdaro <34547598+inchavdaro@users.noreply.github.com>
2021-05-24 22:44:53 +0300

    Feature/#10 user model crud (#14)
    * Add test run
    
    * Refactor models
    
    * Add profiles
    
    * Add user crud operation, security config and login implementation
    
    Co-authored-by: inchavdaro <inchavdarov@gmail.com>

--- src/main/java/ccf/project/CrmApplication.java
+++ src/main/java/ccf/project/CrmApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CrmApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(CrmApplication.class, args);
 	}
 
--- src/test/java/ccf/project/CrmApplicationTests.java
+++ src/test/java/ccf/project/CrmApplicationTests.java
@@ -1,36 +1,8 @@
 package ccf.project;
 
-import ccf.project.domain.UserModel;
-import ccf.project.domain.enums.UserRole;
-import ccf.project.repository.UserRepository;
-import org.hibernate.service.spi.InjectService;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CrmApplicationTests {
-
-	@Autowired
-	UserRepository userRepository;
-
-	@Test
-	void contextLoads() {
-
-
-		int j = 0;
-
-		UserModel u1 = new UserModel();
-		u1.setUsername("Ivan");
-		u1.setPass("admin");
-		u1.setRole(UserRole.ADMIN);
-		userRepository.save(u1);
-		UserModel ivan = userRepository.findByUsername("Ivan");
-		Assertions.assertNotNull(ivan);
-		Assertions.assertEquals(ivan.getPass(), "admin");
-
-		int i = 0;
-	}
 
 }
